d69740b0e16599cb72b904eef58fa9a9655f7f2e
Ildar Galeev <KeinAsylum@gmail.com>
2022-11-18 19:56:32 +0300

    Update service template

--- tools/templates/__serviceName__.ts
+++ tools/templates/__serviceName__.ts
@@ -1,27 +1,15 @@
 import { CodegenClient } from './internal/__namespace__-__serviceName__';
+import context from './internal/__namespace__/context';
+import * as service from './internal/__namespace__/gen-nodejs/__serviceName__';
+import * as metadata from './internal/metadata.json';
 
 import { getMethodsMetadata, codegenClientReducer, connect, ConnectOptions } from '__utilsPath__';
-
-const importMetadata = async () => {
-    const metadata: any = await import('./internal/metadata.json');
-    return metadata.default;
-};
-
-const importService = async () =>
-    await import('./internal/__namespace__/gen-nodejs/__serviceName__');
-
-const importContext = async () => {
-    const context = await import('./internal/__namespace__/context');
-    return context.default;
-};
 
 const __serviceName__ = async (options: ConnectOptions): Promise<CodegenClient> => {
     const serviceName = '__serviceName__';
     const namespace = '__namespace__';
-    const service = await importService();
-    const meta = await importMetadata();
+    const meta = (metadata as any).default;
     const connection = await connect(options, service);
-    const context = await importContext();
     const methodsMeta = getMethodsMetadata(meta, namespace, serviceName);
     return methodsMeta.reduce(
         codegenClientReducer<CodegenClient>(connection, meta, namespace, context),
