48d3b5da7a2e15c47e7f334b591aabf7451a2874
Ildar Galeev <KeinAsylum@gmail.com>
2022-11-21 15:55:45 +0300

    Add types

--- tools/cli.js
+++ tools/cli.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const fse = require('fs-extra');
+const glob = require('glob');
 const path = require('path');
 const webpack = require('webpack');
 const rimraf = require('rimraf');
-const CopyPlugin = require('copy-webpack-plugin');
 
 const compileProto = require('./compile-proto');
 const generateServiceTemplate = require('./generate-service-template');
@@ -29,6 +30,79 @@ const clean = async () => {
     await rm(path.resolve('dist'));
 };
 
+const copyTypes = async () =>
+    new Promise((resolve, reject) => {
+        glob(path.resolve('clients/**/*.d.ts'), (err, files) => {
+            if (err) reject(err);
+            for (const file of files) {
+                const parsed = path.parse(file);
+                const output = parsed.dir.replace(path.resolve('clients'), '');
+                fse.copySync(file, path.resolve(`dist/types/${output}/${parsed.base}`));
+                resolve();
+            }
+        });
+    });
+
+const copyMetadata = async () =>
+    fse.copy(path.resolve('clients/internal/metadata.json'), path.resolve('dist/metadata.json'));
+
+const build = async () =>
+    new Promise((resolve, reject) => {
+        webpack(
+            {
+                name: 'thrift-codegen',
+                mode: 'production',
+                entry: path.resolve('clients/Repository.ts'),
+                devtool: false,
+                module: {
+                    rules: [
+                        {
+                            test: /\.ts?$/,
+                            use: [
+                                {
+                                    loader: 'ts-loader',
+                                    options: {
+                                        context: __dirname,
+                                        configFile: path.resolve(__dirname, 'tsconfig.json'),
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+                resolve: {
+                    extensions: ['.ts', '.js'],
+                    alias: {
+                        thrift: path.resolve('node_modules/@vality/woody/dist/thrift'),
+                    },
+                },
+                output: {
+                    filename: '[name].bundle.js',
+                    path: path.resolve('dist'),
+                    globalObject: 'this',
+                    library: {
+                        name: 'thriftCodegen',
+                        type: 'umd',
+                    },
+                },
+            },
+            (err, stats) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                    return;
+                }
+                console.log(
+                    stats.toString({
+                        chunks: false, // Makes the build much quieter
+                        colors: true, // Shows colors in the console
+                    })
+                );
+                resolve();
+            }
+        );
+    });
+
 async function codegenClient() {
     await clean();
     const outputPath = './clients';
@@ -36,67 +110,10 @@ async function codegenClient() {
     await compileProto(outputProtoPath);
     const serviceTemplateConfig = prepareGenerateServiceConfig(outputProtoPath);
     await generateServiceTemplate(serviceTemplateConfig, outputPath);
-    webpack(
-        {
-            name: 'thrift-codegen',
-            mode: 'production',
-            entry: path.resolve('clients/Repository.ts'),
-            devtool: false,
-            module: {
-                rules: [
-                    {
-                        test: /\.ts?$/,
-                        use: [
-                            {
-                                loader: 'ts-loader',
-                                options: {
-                                    context: __dirname,
-                                    configFile: path.resolve(__dirname, 'tsconfig.json'),
-                                },
-                            },
-                        ],
-                    },
-                ],
-            },
-            resolve: {
-                extensions: ['.ts', '.js'],
-                alias: {
-                    thrift: path.resolve('node_modules/@vality/woody/dist/thrift'),
-                },
-            },
-            output: {
-                filename: '[name].bundle.js',
-                path: path.resolve('dist'),
-                globalObject: 'this',
-                library: {
-                    name: 'thriftCodegen',
-                    type: 'umd',
-                },
-            },
-            plugins: [
-                new CopyPlugin({
-                    patterns: [
-                        {
-                            from: path.resolve('clients/internal/metadata.json'),
-                            to: path.resolve('dist'),
-                        },
-                    ],
-                }),
-            ],
-        },
-        (err, stats) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(
-                stats.toString({
-                    chunks: false, // Makes the build much quieter
-                    colors: true, // Shows colors in the console
-                })
-            );
-        }
-    );
+    await fse.copy(path.resolve(__dirname, 'utils'), path.resolve('clients/utils'));
+    await build();
+    await copyMetadata();
+    await copyTypes();
 }
 
 module.exports = codegenClient;
