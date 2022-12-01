65d44b0d6f1ea6c7231b0ab21cdefd12c7ba1be4
Ildar Galeev <KeinAsylum@gmail.com>
2022-12-01 20:38:11 +0300

    Rename export name

--- tools/templates/__exportName__.ts
+++ tools/templates/__exportName__.ts
@@ -1,25 +1,25 @@
-import { CodegenClient as __exportName__Service } from './internal/__namespace__-__serviceName__';
+import { CodegenClient as __exportName__Interface } from './internal/__namespace__-__serviceName__';
 import context from './internal/__namespace__/context';
 import * as service from './internal/__namespace__/gen-nodejs/__serviceName__';
 
 import { getMethodsMetadata, codegenClientReducer, connect } from '__utilsPath__';
 import { ConnectOptions } from '__utilsPath__/types';
 
-export { CodegenClient as __exportName__Service } from './internal/__namespace__-__serviceName__';
+export { CodegenClient as __exportName__Interface } from './internal/__namespace__-__serviceName__';
 
-export const __exportName__ = async (options: ConnectOptions): Promise<__exportName__Service> => {
+export const __exportName__ = async (options: ConnectOptions): Promise<__exportName__Interface> => {
     const serviceName = '__serviceName__';
     const namespace = '__namespace__';
     const path = options.path || '__connectionPath__';
     const connection = await connect(path, service, options.headers, options.deadlineConfig);
     const methodsMeta = getMethodsMetadata(options.metadata, namespace, serviceName);
     return methodsMeta.reduce(
-        codegenClientReducer<__exportName__Service>(
+        codegenClientReducer<__exportName__Interface>(
             connection,
             options.metadata,
             namespace,
             context
         ),
-        {} as __exportName__Service
+        {} as __exportName__Interface
     );
 };
