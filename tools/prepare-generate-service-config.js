f0613b91e13e09df8e2245887943ab8fd5555d6c
Ildar Galeev <KeinAsylum@gmail.com>
2022-12-21 17:12:52 +0300

    Namespace service identity

--- tools/prepare-generate-service-config.js
+++ tools/prepare-generate-service-config.js
@@ -1,28 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const camelCase = require('lodash/camelCase');
-
-const duplicatedNamespaceServiceNameReducer = (config, configCurrEl) => {
-    const result = {
-        ...configCurrEl,
-        exportName:
-            configCurrEl.namespace === configCurrEl.serviceName.toLowerCase()
-                ? `${configCurrEl.serviceName}Service`
-                : configCurrEl.serviceName,
-    };
-    return [...config, result];
-};
-
-const duplicatedServiceNamesReducer = (config, configCurrEl) => {
-    const duplicate = config.find(({ serviceName }) => serviceName === configCurrEl.serviceName);
-    const result = {
-        ...configCurrEl,
-        exportName: duplicate
-            ? camelCase(`${camelCase(configCurrEl.namespace)}${configCurrEl.serviceName}`)
-            : configCurrEl.serviceName,
-    };
-    return [...config, result];
-};
 
 const namespacesReducer = (includedNamespaces) => (config, configCurrEl) =>
     includedNamespaces.includes(configCurrEl.namespace) ? [...config, configCurrEl] : [...config];
@@ -41,11 +18,8 @@ const prepareGenerateServiceConfig = (compiledDist, includedNamespaces) =>
             return {
                 namespace,
                 serviceName,
-                exportName: serviceName,
+                exportName: `${namespace}_${serviceName}`,
             };
         })
-        .reduce(duplicatedServiceNamesReducer, [])
-        .reduce(duplicatedNamespaceServiceNameReducer, [])
         .reduce(namespacesReducer(includedNamespaces), []);
-
 module.exports = prepareGenerateServiceConfig;
