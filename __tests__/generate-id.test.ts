b566ce745ddff7fbeaa1c13aea735c86c3061f12
Ildar Galeev <KeinAsylum@gmail.com>
2023-10-30 18:53:18 +0300

    Add generate id util fn

---
+++ __tests__/generate-id.test.ts
@@ -0,0 +1,26 @@
+import { generateId } from '../tools/utils/generate-id';
+
+describe('generateId function', () => {
+    it('should return a string', () => {
+        const id = generateId();
+        expect(typeof id).toBe('string');
+    });
+
+    it('should return a unique ID each time it is called', () => {
+        const id1 = generateId();
+        const id2 = generateId();
+        expect(id1).not.toBe(id2);
+    });
+
+    it('should return a base64 encoded string', () => {
+        const id = generateId();
+        const isBase64 = /^[A-Za-z0-9+/]+[=]{0,2}$/.test(id);
+        expect(isBase64).toBe(true);
+    });
+
+    it('should have a consistent length', () => {
+        const id1 = generateId();
+        const id2 = generateId();
+        expect(id1.length).toBe(id2.length);
+    });
+});
---
+++ tools/utils/generate-id.ts
@@ -0,0 +1,15 @@
+import FlakeId from 'flake-idgen';
+import baseX from 'base-x';
+
+const bs64 = baseX('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/');
+const flake = new FlakeId();
+
+/**
+ * Generates a unique ID using FlakeId and encodes it in base64.
+ *
+ * Is used for generating unique identifiers for
+ * tracing requests, like span IDs and trace IDs.
+ *
+ * @returns {string} - The base64-encoded unique ID.
+ */
+export const generateId = (): string => bs64.encode(flake.next());
