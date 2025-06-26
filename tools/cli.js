fa006dbf439566403c0da14c1909e8e0ff43f094
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2025-06-27 03:52:08 +0500

    refactor: enhance copyMetadataAsTsFile function to accept parameters for improved flexibility

--- tools/cli.js
+++ tools/cli.js
@@ -11,17 +11,25 @@ const prepareGenerateServiceConfig = require('./prepare-generate-service-config'
 
 const clean = () => shelljs.rm('-rf', path.resolve('clients'), path.resolve('dist'));
 
-const copyMetadataAsTsFile = async () => {
-    const metadataJsonPath = path.resolve('clients/internal/metadata.json');
-    const metadataTsPath = path.resolve('clients/internal/metadata.ts');
+const copyMetadataAsTsFile = async (outputProtoPath, metadataName) => {
+    const metadataJsonPath = path.resolve(path.join(outputProtoPath, `${metadataName}.json`));
+    const metadataTsPath = path.resolve(path.join(outputProtoPath, `${metadataName}.ts`));
     const content = await fse.readFile(metadataJsonPath, 'utf-8');
-    await fse.outputFile(metadataTsPath, `export const metadata: any[] = ${content}\n`);
+    await fse.outputFile(
+        metadataTsPath,
+        `import { ThriftAstMetadata } from '../utils/types';\n\n` +
+            `export const metadata: ThriftAstMetadata[] = ${content};\n`,
+    );
 };
 
 const copyTsUtils = async () =>
     fse.copy(path.resolve(__dirname, 'utils'), path.resolve('clients/utils'));
 
 async function codegenClient() {
+    const outputPath = './clients';
+    const outputProtoPath = `${outputPath}/internal`;
+    const metadataName = 'metadata';
+
     const argv = yargs(hideBin(process.argv)).options({
         inputs: {
             alias: 'i',
@@ -37,8 +45,6 @@ async function codegenClient() {
         },
     }).argv;
     clean();
-    const outputPath = './clients';
-    const outputProtoPath = `${outputPath}/internal`;
     await compileProto(argv.inputs, outputProtoPath);
     const serviceTemplateConfig = prepareGenerateServiceConfig(outputProtoPath, argv.namespaces);
     const metadata = JSON.parse(
@@ -46,7 +52,7 @@ async function codegenClient() {
     );
     await generateServiceTemplate(serviceTemplateConfig, argv.namespaces, outputPath, metadata);
     await copyTsUtils();
-    await copyMetadataAsTsFile();
+    await copyMetadataAsTsFile(outputProtoPath, metadataName);
     await build({
         entry: [path.resolve(path.join(outputPath, 'index.ts'))],
         sourcemap: true,
